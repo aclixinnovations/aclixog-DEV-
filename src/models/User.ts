23ea2a29061e50545edaa40794282dc15c9ae8f1
aclixinnovations <aclixinnovations@gmail.com>
2025-10-25 11:19:42 +0530

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,26 @@
-import mongoose, { Schema, models, model } from "mongoose";
+import mongoose, { Schema, Document, models } from "mongoose";
 
-const UserSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["admin", "user"], default: "user" },
-}, { timestamps: true });
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: "admin" | "manager" | "user";
+}
 
-const User = models.User || model("User", UserSchema);
+const UserSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "manager", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || mongoose.model<IUser>("User", UserSchema);
+
 export default User;
